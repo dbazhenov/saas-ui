d29ae09611b210d0552a70ca381f7f3baf42ccd9
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-04-25 21:35:49 +0300

    SAAS-19 add sentry integration (#217)
    * SAAS-19 add sentry integration
    
    * SAAS-19 add enviroment name to sentry config

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -4,6 +4,8 @@ export const PRIVACY_PMM_URL = 'https://per.co.na/pmm/platform-privacy';
 
 export const DOWNLOAD_PMM_LINK = 'https://www.percona.com/downloads/pmm2';
 
+export const SENTRY_DSN = 'https://fe7cfc4487d846d58d0273bd2be7dc91@o1209311.ingest.sentry.io/6361088';
+
 export const PORTAL_HOST = window.location.host;
 
 export const PORTAL_ORIGIN = window.location.origin;
@@ -31,3 +33,8 @@ export const MAX_NAME_LENGTH = 50;
 export const PASSWORD_MIN_LENGTH = 10;
 
 export const OKTA_TOKEN_STORE_LOCALSTORAGE_KEY = 'okta-token-storage';
+
+// eslint-disable-next-line no-magic-numbers
+export const SENTRY_TRACES_SAMPLE_RATE = IS_PRODUCTION ? 0.5 : 1.0;
+
+export const ENVIRONMENT = IS_PRODUCTION ? 'production' : 'development';
--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,13 @@ import { css } from 'emotion';
 import { Security } from '@okta/okta-react';
 import { toRelativeUrl } from '@okta/okta-auth-js';
 import { ThemeContext } from '@grafana/ui';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import { Main } from 'components';
 import { store } from 'store';
 import { getCurrentTheme } from 'store/theme';
 import { history, oktaAuth } from 'core';
+import { ENVIRONMENT, SENTRY_DSN, SENTRY_TRACES_SAMPLE_RATE } from 'core/constants';
 
 import 'react-toastify/dist/ReactToastify.min.css';
 import './styles/global.css';
@@ -19,6 +22,15 @@ import './styles/global.css';
  * way to get faSpinner icon rendered other than to import fontawesome :|
  */
 import './styles/font-awesome.css';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  environment: ENVIRONMENT,
+
+  // This is meant to capture 100% for dev and a much lower rate for prod
+  tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
+});
 
 const onAuthRequired = () => {
   history.push('/login');
@@ -38,31 +50,27 @@ const App = () => {
   const theme = useSelector(getCurrentTheme);
 
   return (
-      <ThemeContext.Provider value={theme}>
-        <ConnectedRouter context={ReactReduxContext} history={history}>
-          <Security
-            oktaAuth={oktaAuth}
-            onAuthRequired={onAuthRequired}
-            restoreOriginalUri={restoreOriginalUri}
-          >
-            <Main />
-          </Security>
-        </ConnectedRouter>
-        <ToastContainer
-          bodyClassName={css`
-            padding: 0.5em;
-          `}
-          position={toast.POSITION.TOP_RIGHT}
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          transition={Slide}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </ThemeContext.Provider>
+    <ThemeContext.Provider value={theme}>
+      <ConnectedRouter context={ReactReduxContext} history={history}>
+        <Security oktaAuth={oktaAuth} onAuthRequired={onAuthRequired} restoreOriginalUri={restoreOriginalUri}>
+          <Main />
+        </Security>
+      </ConnectedRouter>
+      <ToastContainer
+        bodyClassName={css`
+          padding: 0.5em;
+        `}
+        position={toast.POSITION.TOP_RIGHT}
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        transition={Slide}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </ThemeContext.Provider>
   );
 };
 
