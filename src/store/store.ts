b4e11bda690e5f67bfa2adc2cc5b889c4f3c65f9
Dora <103416234+doracretu3pillar@users.noreply.github.com>
2023-03-17 15:50:31 +0200

    Saas 1101 redundant error messages (#404)
    * SASS-1101 Added interceptor in api.service.ts
    
    Moved the check for state.email from store.ts to an axios interceptor in api.service.ts
    
    * SAAS-1101 Removed interceptor, added logic inside SecureRoute
    
    Removed interceptor, added logic inside SecureRoute, reverted all changes
    
    * SASS-1101 Show error inside the console
    
    In case the getUser or the dispatch throws an error, show it on the console
    
    * SASS-1101 Fix after review
    
    * SAAS-1101 Send userInfo as props to MarketingBanner
    
    * SAAS-1101 Fix for userInfo type
    
    * SAAS-1101 Small refactor
    
    Added parameters types in MarketingBanner declaration
    
    * SASS-1101 Configuration for test store and tests
    
    https://robertmarshall.dev/blog/arrange-act-and-assert-pattern-the-three-as-of-unit-testing/
    
    https://redux.js.org/usage/writing-tests
    
    * SASS-1101 Added test for MarketingBanner
    
    Added test for MarketingBanner using TestContainer, and reverted all other changes
    
    * SASS-1101 Refactor MarketingBanner.test
    
    * Longer duration for the marketing consent to be hidden
    
    * Fix for the wrong title in the UI tests
    
    * Skips deprecated marketing test.
    
    * SAAS-1101 Fixed updateUserInfoAction object values
    
    ---------
    
    Co-authored-by: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Co-authored-by: Peter <sirotnak.p@gmail.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,56 +1,23 @@
-import { AnyAction, configureStore } from '@reduxjs/toolkit';
-import { toast } from 'react-toastify';
-import { oktaAuth } from 'core';
-import { Messages } from 'core/api';
+import { configureStore } from '@reduxjs/toolkit';
 import { loadState } from 'store/persistence/engine';
 import { kubernetesApi } from 'pages/K8sClusterCreation/K8sClusterCreation.service';
 import { authApi } from 'core/api/auth.service';
 import { eventsApi } from 'core/api/events.service';
 import { membersListApi } from 'pages/ManageOrganization/MembersList/MembersList.service';
-import { errorUserInfoAction, getAuth, startUserInfoAction, updateUserInfoAction } from './auth';
 import { rootReducer } from './reducers';
 
 const preloadedState = loadState();
-
-/**
- * This middleware makes sure to call userInfo and wait until it completes to persist the user data.
- * This way we respect DRY in API calls, which otherwise would have to implement this behavior.
- */
-export const authMiddleware = (store: any) => (next: any) => (action: AnyAction) => {
-  const token = oktaAuth.getAccessToken();
-
-  // We should only call .getUser when the user is authenticated, or it will fail
-  if (!token || action.type?.startsWith('@@')) {
-    return next(action);
-  }
-
-  const auth = getAuth(store.getState());
-
-  if (!auth.pending && !auth.email) {
-    return Promise.resolve()
-      .then(() => next(startUserInfoAction()))
-      .then(() => oktaAuth.getUser())
-      .then(({ email, family_name: lastName, given_name: firstName }) =>
-        next(updateUserInfoAction({ email, firstName, lastName })),
-      )
-      .catch((err) => {
-        toast.error(Messages.genericAPIFailure);
-        console.error(err);
-        next(errorUserInfoAction());
-      })
-      .finally(() => next(action));
-  }
-
-  return next(action);
-};
 
 export const store = configureStore({
   reducer: rootReducer,
   preloadedState,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .prepend(authMiddleware)
-      .concat(authApi.middleware, membersListApi.middleware, kubernetesApi.middleware, eventsApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      membersListApi.middleware,
+      kubernetesApi.middleware,
+      eventsApi.middleware,
+    ),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
